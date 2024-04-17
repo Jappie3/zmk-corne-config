3aaed9b1e33e464412097d164c22c0bcceff09da
Jappie3 <jasper22034@gmail.com>
2024-04-18 01:10:29 +0200

    feat: add zmk west manifests

---
+++ zmk/nix_inputs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+# deps: west, awk, sed, ripgrep
+
+# make sure you have the following files from the zephyrproject-rtos/zephyr repo in the zephyr/ directory:
+# zephyr/
+# ├── submanifests
+# │   └── optional.yaml
+# └── west.yml
+
+WEST_MANIFEST="$(west manifest --resolve)"
+ALL_NAMES=()
+
+while read -r url rev; do
+    # filter slashes & .git at the end from url to create slug
+    slug="$(echo $url | cut -d'/' -f 4- | sed 's./._.' | sed 's/.git//')"
+    ALL_NAMES+=("\${$slug};")
+
+    # TODO remove .git from url
+    # echo "$slug = builtins.fetchGit { url = \"$url\"; rev = \"$rev\"; };"
+
+    # https://github.com/<org|user>/<repo>/archive/<commit>.tar.gz
+    tarball_url="$(echo $url | sed 's/\.git//')/archive/$rev.tar.gz"
+    echo "$slug = builtins.fetchTarball { url = \"$tarball_url\"; sha256 = \"sha256:$(nix-prefetch-url --unpack $tarball_url --quiet)\"; };"
+    #wget $tarball_url -O- --quiet | sha256sum | cut -d' ' -f1
+
+done < <(awk '
+ /name:/ { name=$2 }
+ /url:/ { url=$2 }
+ /revision:/ { revision=$2; print url, revision }
+' <<EOF 
+$WEST_MANIFEST
+EOF
+)
+
+echo -e "\n\n\n${ALL_NAMES[@]}"
