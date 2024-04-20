ca4c7df1fd353066c94603e2c7cb0cf29642d8c9
Jappie3 <jasper22034@gmail.com>
2024-04-20 19:07:45 +0200

    style: cleanup code & comments

--- zmk-zephyr/nix_inputs.sh
+++ zmk-zephyr/nix_inputs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# deps: west, awk, sed, ripgrep
+# deps: west, awk, sed
 
 # make sure you have the following files from the https://github.com/zmkfirmware/zephyr/tree/v3.5.0%2Bzmk-fixes/ repo in the zephyr/ directory:
 # zephyr/
@@ -14,12 +14,10 @@ WEST_MANIFEST="$(west manifest --resolve)"
 ALL_NAMES=()
 
 while read -r url rev; do
+
     # filter slashes & .git at the end from url to create slug
     slug="$(echo $url | cut -d'/' -f 4- | sed 's./._.' | sed 's/.git//')"
     ALL_NAMES+=("\${$slug};")
-
-    # TODO remove .git from url
-    # echo "$slug = builtins.fetchGit { url = \"$url\"; rev = \"$rev\"; };"
 
     # https://github.com/<org|user>/<repo>/archive/<commit>.tar.gz
     tarball_url="$(echo $url | sed 's/\.git//')/archive/$rev.tar.gz"
